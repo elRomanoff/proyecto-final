6d7e4f2691dbd3b17abbb3861dae61e7de347169
elRomanoff <83791949+elRomanoff@users.noreply.github.com>
2021-11-25 15:31:10 -0300

    Update product.js

--- src/routers/product.js
+++ src/routers/product.js
@@ -3,36 +3,40 @@ const productRouter = express.Router();
 const ProductDaoMongo = require("../daos/ProductDaoMongo.js")
 const isAdmin = require('../middlewares/isAdmin.js')
 const ProductDaoFire = require("../containers/FireContainer.js")
+require ("dotenv").config()
 
-const productDaoMongo = new ProductDaoMongo
+let productDao
+
+if(process.env.DATABASE === "MONGODB") productDao = new ProductDaoMongo
+else if (process.env.DATABASE === "FIREBASE") productDao = new ProductDaoFire("https://coderhouse-ecommerce-uwu-default-rtdb.firebaseio.com/")
 
 productRouter.get('/', async (req,res) =>{
-    const data = await productDaoMongo.getAll();
+    const data = await productDao.getAll();
     res.send(data)
 })
 
 productRouter.get("/:id", async(req,res) =>{
-    const data = await productDaoMongo.getById(req.params.id);
+    const data = await productDao.getById(req.params.id);
     res.send(data)
 })
 
 productRouter.delete("/", async(req,res) =>{
-    const data = await productDaoMongo.deleteAll();
+    const data = await productDao.deleteAll();
     res.send(data);
 })
 productRouter.delete("/:id", async(req, res) =>{
-    const data = await productDaoMongo.deleteById(req.params.id)
+    const data = await productDao.deleteById(req.params.id)
     res.send(data)
 })
 productRouter.post("/", async(req,res) =>{
     let product = req.body;
-    const data = productDaoMongo.save(product);
+    const data = productDao.save(product);
     res.send(data)
 })
 productRouter.put("/:id", async(req, res) =>{
     const product = req.body;
     const id = req.params.id;
-    const data = productDaoMongo.update(id, product);
+    const data = productDao.update(id, product);
     res.send(data)
 })
 
